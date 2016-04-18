690c1d04a293e92b99ccc549691e532c9488302c
alu0100622492 <alu0100622492@ull.edu.es>
2016-04-18 16:00:39 +0100

    viendose el index en la app

--- models/calculate.js
+++ models/calculate.js
@@ -1,7 +1,7 @@
 (() => {
 "use strict";
 
-const regexp = /XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/g;
+const regexp = /"((?:[^"\\]|\\.)*)"|([^,\s]+)|,\s*(?=,|$)|^\s*,/g;
 const calculate = (original) => {
     let lines = original.split(/\n+\s*/);
     let commonLength = lines[0].match(regexp).length;
@@ -20,7 +20,7 @@ const calculate = (original) => {
       let error = false;
 
       // skip empty lines and comments
-      if (temp.match(/(^\s*$)|(^#.*)/)) continue; 
+      if (temp.match(/(^\s*$)|(^#.*)/)) continue;
       if (m) {
         result = m.map(removeQuotes);
         error = (commonLength != m.length);
@@ -35,5 +35,5 @@ const calculate = (original) => {
     return r;
   };
 
-module.exports = XXXXXXXXXX
+module.exports = calculate;
 })();
